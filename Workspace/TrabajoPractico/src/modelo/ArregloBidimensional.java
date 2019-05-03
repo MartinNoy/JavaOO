2052bd2643fb517b77d5610fa810f5a64d63e361
MartinNoy <Martinnoy98@hotmail.com>
2019-05-03 20:48:27 -0300

    datetime

--- Workspace/TrabajoPractico/src/modelo/ArregloBidimensional.java
+++ Workspace/TrabajoPractico/src/modelo/ArregloBidimensional.java
@@ -1,5 +1,7 @@
 package modelo;
 
+import java.util.Arrays;
+
 public class ArregloBidimensional {
 	private double[][] matrizA;
 
@@ -16,9 +18,18 @@ public class ArregloBidimensional {
 		this.matrizA = matrizA;
 	}
 	
-	public sumar(double[][] matrizB) {
-		double[][] resultado;
-		for (int i=0; i<matrizA.length;i++) {
+	public void imprimirMatriz(double[][]Matriz) {
+		for (int i=0 ; i<Matriz.length;i++) {
+			for (int j=0 ; j<Matriz.length ; j++) {
+				System.out.println(Matriz[i][j]);
+			}
+		}
+	}
+	
+
+	public double[][] sumar(double[][] matrizB) {
+		double[][] resultado = new double [matrizB.length][matrizB[0].length];
+		for (int i=0; i<matrizB.length;i++) {
 			for(int j=0; j<matrizB[0].length;i++) {
 				resultado[i][j]=matrizA[i][j]+matrizB[i][j];
 			}
@@ -26,7 +37,28 @@ public class ArregloBidimensional {
 		return resultado;
 	}
 	
+	public double[][] restar(double[][] matrizB) {
+		double[][] resultado = new double [matrizB.length][matrizB[0].length];
+		for (int i=0; i<matrizB.length;i++) {
+			for(int j=0; j<matrizB[0].length;i++) {
+				resultado[i][j]=matrizA[i][j]-matrizB[i][j];
+			}
+		}
+		return resultado;
+	}
 	
+	
+	public double[][] traspuesta(){
+		double[][] transpuesta = new double [matrizA[0].length][matrizA.length];
+		
+		for (int i = 0; i<=matrizA[0].length;i++) {
+			for (int j = 0; j<=matrizA.length; j++) {
+				transpuesta[i][j] = matrizA[j][i];
+			}
+		}
+		return transpuesta;
+	}
+ 	
 	
 
 }
